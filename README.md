587eb9c7a7594b512171691f079642da3212d12b
Solidras <solidras@gmail.com>
2020-06-28 09:57:28 +0200

    Initial bot commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # IdolsBot
  
+## Creating necessary files
+You need to create one .env file to be able to launch the bot with the application you created on [Discord Developers Portal](https://discord.com/developers/applications).
+This file must be named *.env* and placed in *src/* folder. It should contains one line with the bot token.
+### Example 
+```
+# .env file
+BOT_TOKEN=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+```
---
+++ src/main.py
@@ -0,0 +1,49 @@
+import os
+import asyncio
+import discord
+
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+bot = commands.Bot(command_prefix='*')
+
+
+#### Bot commands ####
+
+@bot.command()
+async def ping(ctx):
+    await ctx.message.delete()
+    await ctx.send('Yup, I\'m awake.', delete_after=5)
+
+
+#### Bot event handlers ####
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.send('Unknown command.')
+        return
+    elif isinstance(error, commands.CheckFailure):
+        await ctx.send("You're not authorized to execute this command.")
+        return
+    elif isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send("Missing arguments. !help for display help")
+        return
+    raise error
+
+
+#### Utilities functions ####
+
+
+#### Launch bot ####
+
+bot.run(BOT_TOKEN)
