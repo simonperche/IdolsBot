833037be74c41335d9d8787f60ae969753ad8064
Solidras <solidras@gmail.com>
2020-07-07 18:14:44 +0200

    Add time command
    Allow users to see rolls and claim reset time

--- src/profile.py
+++ src/profile.py
@@ -1,8 +1,10 @@
+from datetime import datetime
+
 import discord
 from discord.ext import commands
 
 from database import DatabaseDeck, DatabaseIdol
-
+from roll import min_until_next_claim
 
 class Profile(commands.Cog):
     def __init__(self, bot):
@@ -27,3 +29,36 @@ class Profile(commands.Cog):
         embed.set_thumbnail(url=user.avatar_url)
 
         await ctx.send(embed=embed)
+
+    @commands.command(aliases=['tu'], description='Show time before next rolls and claim reset.')
+    async def time(self, ctx):
+        next_claim = min_until_next_claim(ctx.guild.id, ctx.author.id)
+
+        username = ctx.author.name if ctx.author.nick is None else ctx.author.nick
+
+        msg = f'{username}, you '
+        if next_claim == 0:
+            msg += f'can claim right now!'
+        else:
+            time = divmod(next_claim, 60)
+            msg += f'can\'t claim for another **' + \
+                   (str(time[0]) + 'h ' if time[0] != 0 else '') + f'{str(time[1])} min**.'
+
+        user_nb_rolls = DatabaseDeck.get().get_nb_rolls(ctx.guild.id, ctx.author.id)
+        max_rolls = DatabaseDeck.get().get_rolls_per_hour(ctx.guild.id)
+
+        last_roll = DatabaseDeck.get().get_last_roll(ctx.guild.id, ctx.author.id)
+        if not last_roll:
+            user_nb_rolls = 0
+        else:
+            last_roll = datetime.strptime(last_roll, '%Y-%m-%d %H:%M:%S')
+            now = datetime.now()
+
+            # If a new hour began
+            if now.date() != last_roll.date() or (now.date() == last_roll.date() and now.hour != last_roll.hour):
+                user_nb_rolls = 0
+
+        msg += f'\nYou have **{max_rolls - user_nb_rolls}** rolls left.\n' \
+               f'Next rolls reset in **{60 - datetime.now().minute} min**.'
+
+        await ctx.send(msg)
